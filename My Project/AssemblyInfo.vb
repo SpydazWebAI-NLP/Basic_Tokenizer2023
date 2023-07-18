f0a65e0264b40ca6a58fdc1748a3204948bf79ab
spydaz <LeroySamuelDyer@hotmail.co.uk>
2023-07-18 11:09:42 +0300

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SpydazWebNLP - Basic Tokenizer 2023")>
+<Assembly: AssemblyDescription("Updated version of tokenizer - With Char/Word/Sentence Tokenizations , Featuring ,  FreqNgrams, SubWord Tokenization, PMI Scoring, Document Vocabulary Creation, Model Export.")>
+<Assembly: AssemblyCompany("SpydazWebAI Creations")>
+<Assembly: AssemblyProduct("Basic Tokenizer 2023")>
+<Assembly: AssemblyCopyright("Copyright © SpydazWebAI 2023")>
+<Assembly: AssemblyTrademark("TM© SpydazWebAI 2023")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("39f8aecf-c134-4682-bd94-aa11e3db23eb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
